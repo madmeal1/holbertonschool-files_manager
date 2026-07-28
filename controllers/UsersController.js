8d13369d4b59397fcbd5f65f13b2b2f6bf75f659
madmeal1 <mael.salet33@gmail.com>
2026-07-28 14:23:15 +0200

    [feat] add user authentication with token

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,7 @@
-// controllers/UsersController.js
 import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(req, res) {
@@ -31,6 +32,30 @@ class UsersController {
       id: result.insertedId,
       email,
     });
+  }
+
+  static async getMe(req, res) {
+    const token = req.header('X-Token');
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const usersCollection = dbClient.db.collection('users');
+    const user = await usersCollection.findOne({ _id: ObjectId(userId) });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
   }
 }
 
